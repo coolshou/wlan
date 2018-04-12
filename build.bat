336abd9e3c81e088b76c18249ec9c17c274fc264
jimmy <jimmy_yeh@alphanetworks.com>
2018-04-12 13:16:23 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,43 @@
+@echo OFF
 
+set BAT=%0
+set sCMD=%1
 
-REM x86
+echo cmd %sCMD%
+if "%sCMD%" NEQ "" (
+  if "%sCMD%" NEQ "build" (
+    call :Clean
+  ) else (
+    call :Build
+  )
+) else (
+  call :Help
+)
+exit /b
+
+:Help
+echo =====%BAT% usage===============
+echo  %BAT% build : build x86/x64 
+echo  %BAT%  : clean build
+echo ===================================
+exit /b
+
+:Build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
-MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=Win32
 MSBuild wlan.sln /t:Rebuild /p:Configuration=Release;Platform=Win32
 mt -manifest "wlan.exe.manifest"  -outputresource:"Win32\Release\wlan.exe;#1"
-REM x64
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
-MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=x64
 MSBuild wlan.sln /t:Rebuild /p:Configuration=Release;Platform=x64
 mt -manifest "wlan.exe.manifest"  -outputresource:"x64\Release\wlan.exe;#1"
+
+exit /b
+
+:Clean
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=Win32
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=x64
+
+exit /b
