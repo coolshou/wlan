0762a765818d7555d30cc5ec8cb9723a4d92cdb6
jimmy <jimmy_yeh@alphanetworks.com>
2018-11-20 14:02:05 +0800

    change to vs2015

--- build.bat
+++ build.bat
@@ -23,21 +23,25 @@ echo ===================================
 exit /b
 
 :Build
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
-MSBuild wlan.sln /t:Rebuild /p:Configuration=Release;Platform=Win32
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+MSBuild wlan.sln /m /t:Rebuild /p:Configuration=Release;Platform=Win32
 mt -manifest "wlan.exe.manifest"  -outputresource:"Win32\Release\wlan.exe;#1"
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
-MSBuild wlan.sln /t:Rebuild /p:Configuration=Release;Platform=x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+MSBuild wlan.sln /m /t:Rebuild /p:Configuration=Release;Platform=x64
 mt -manifest "wlan.exe.manifest"  -outputresource:"x64\Release\wlan.exe;#1"
 
 exit /b
 
 :Clean
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
-MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=Win32
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+MSBuild wlan.sln /m /t:clean /p:Configuration=Release;Platform=Win32
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
-MSBuild wlan.sln /t:clean /p:Configuration=Release;Platform=x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+MSBuild wlan.sln /m /t:clean /p:Configuration=Release;Platform=x64
 
 exit /b
