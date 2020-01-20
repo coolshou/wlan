888423bd1ab29d3f567328e2c0f870a0cdd13481
jimmy <jimmy_yeh@alphanetworks.com>
2020-01-20 09:53:28 +0800

    change to chcp 65001

--- build.bat
+++ build.bat
@@ -24,13 +24,13 @@ exit /b
 
 :Build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 MSBuild wlan.sln /m /t:Rebuild /p:Configuration=Release;Platform=Win32
 mt -manifest "wlan.exe.manifest"  -outputresource:"Win32\Release\wlan.exe;#1"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 MSBuild wlan.sln /m /t:Rebuild /p:Configuration=Release;Platform=x64
 mt -manifest "wlan.exe.manifest"  -outputresource:"x64\Release\wlan.exe;#1"
@@ -39,12 +39,12 @@ exit /b
 
 :Clean
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 MSBuild wlan.sln /m /t:clean /p:Configuration=Release;Platform=Win32
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 MSBuild wlan.sln /m /t:clean /p:Configuration=Release;Platform=x64
 
